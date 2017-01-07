ef2beb6962b257ffd37a56dad7d983f1b9cbd545
Badb <Badb@BADB>
2017-01-08 00:40:10 +0800

    加入專案檔案。

---
+++ DriverInstaller/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace DriverInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lab_ReadMe = new System.Windows.Forms.Label();
+            this.btn_sel = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.text_FilePatSel = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Item_Dev = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Item_Path = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Item_Del = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.pictureBox_processing = new System.Windows.Forms.PictureBox();
+            this.label_ProcessStatus = new System.Windows.Forms.Label();
+            this.label_List = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_processing)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lab_ReadMe
+            // 
+            this.lab_ReadMe.AutoSize = true;
+            this.lab_ReadMe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_ReadMe.Location = new System.Drawing.Point(12, 18);
+            this.lab_ReadMe.Name = "lab_ReadMe";
+            this.lab_ReadMe.Size = new System.Drawing.Size(356, 20);
+            this.lab_ReadMe.TabIndex = 0;
+            this.lab_ReadMe.Text = "Click \"Select\" to select driver zip, then click \"Add\".";
+            // 
+            // btn_sel
+            // 
+            this.btn_sel.Location = new System.Drawing.Point(315, 76);
+            this.btn_sel.Name = "btn_sel";
+            this.btn_sel.Size = new System.Drawing.Size(75, 23);
+            this.btn_sel.TabIndex = 1;
+            this.btn_sel.Text = "Select";
+            this.btn_sel.UseVisualStyleBackColor = true;
+            this.btn_sel.Click += new System.EventHandler(this.btn_sel_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(396, 76);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // text_FilePatSel
+            // 
+            this.text_FilePatSel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_FilePatSel.Location = new System.Drawing.Point(9, 44);
+            this.text_FilePatSel.Name = "text_FilePatSel";
+            this.text_FilePatSel.ReadOnly = true;
+            this.text_FilePatSel.Size = new System.Drawing.Size(463, 23);
+            this.text_FilePatSel.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Item_Dev,
+            this.Item_Path,
+            this.Item_Del});
+            this.dataGridView1.Location = new System.Drawing.Point(9, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(463, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Item_Dev
+            // 
+            this.Item_Dev.HeaderText = "Device";
+            this.Item_Dev.Name = "Item_Dev";
+            this.Item_Dev.ReadOnly = true;
+            // 
+            // Item_Path
+            // 
+            this.Item_Path.HeaderText = "Path";
+            this.Item_Path.Name = "Item_Path";
+            this.Item_Path.ReadOnly = true;
+            this.Item_Path.Width = 290;
+            // 
+            // Item_Del
+            // 
+            this.Item_Del.HeaderText = "Del";
+            this.Item_Del.Name = "Item_Del";
+            this.Item_Del.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Item_Del.Width = 30;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Start.Location = new System.Drawing.Point(394, 277);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 4;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // pictureBox_processing
+            // 
+            this.pictureBox_processing.Image = global::DriverInstaller.Properties.Resources.oie_5173511yFNPNZCI;
+            this.pictureBox_processing.Location = new System.Drawing.Point(196, -1);
+            this.pictureBox_processing.Name = "pictureBox_processing";
+            this.pictureBox_processing.Size = new System.Drawing.Size(104, 85);
+            this.pictureBox_processing.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_processing.TabIndex = 0;
+            this.pictureBox_processing.TabStop = false;
+            this.pictureBox_processing.Visible = false;
+            // 
+            // label_ProcessStatus
+            // 
+            this.label_ProcessStatus.AutoSize = true;
+            this.label_ProcessStatus.Location = new System.Drawing.Point(193, 90);
+            this.label_ProcessStatus.Name = "label_ProcessStatus";
+            this.label_ProcessStatus.Size = new System.Drawing.Size(0, 13);
+            this.label_ProcessStatus.TabIndex = 5;
+            this.label_ProcessStatus.Visible = false;
+            // 
+            // label_List
+            // 
+            this.label_List.AutoSize = true;
+            this.label_List.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_List.Location = new System.Drawing.Point(12, 97);
+            this.label_List.Name = "label_List";
+            this.label_List.Size = new System.Drawing.Size(93, 17);
+            this.label_List.TabIndex = 6;
+            this.label_List.Text = "Selected List:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(481, 312);
+            this.Controls.Add(this.label_List);
+            this.Controls.Add(this.label_ProcessStatus);
+            this.Controls.Add(this.pictureBox_processing);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.text_FilePatSel);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_sel);
+            this.Controls.Add(this.lab_ReadMe);
+            this.MaximumSize = new System.Drawing.Size(497, 351);
+            this.MinimumSize = new System.Drawing.Size(497, 351);
+            this.Name = "Form1";
+            this.Text = "Driver Installer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_processing)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab_ReadMe;
+        private System.Windows.Forms.Button btn_sel;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox text_FilePatSel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Item_Dev;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Item_Path;
+        private System.Windows.Forms.DataGridViewButtonColumn Item_Del;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.PictureBox pictureBox_processing;
+        private System.Windows.Forms.Label label_ProcessStatus;
+        private System.Windows.Forms.Label label_List;
+    }
+}
+
---
+++ DriverInstaller/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DriverInstaller
+{
+    public partial class Form1 : Form
+    {
+        public string _FilepathSel, _Floderpath, _InstallerExeFile, _DeviceName, _ZipPath, _Unzipto = null;
+        public DataTable dt;
+        public string[] _StrFilter = { "wifi", "bt", "wlan", "bluetooth"};
+        public BackgroundWorker backgroundWorker;
+        public Form1()
+        {
+            InitializeComponent();
+            //Create Data table
+            dt = new DataTable("Driverlist");
+            dt.Columns.Add("ItemNumber", typeof(Byte));
+            dt.Columns.Add("DriverPath", typeof(String));
+            //Arrange file path length
+            dt.Columns["ItemNumber"].Unique = true;
+            dt.Columns["DriverPath"].MaxLength = 1000;
+            dt.Columns["DriverPath"].AllowDBNull = false;
+
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += backgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += backgroundWorker_ProgressChanged;
+            backgroundWorker.RunWorkerCompleted += backgroundWorker_RunWorkerCompleted;
+        }
+        //Get Zip File Path function
+        private string _GetFilePath(string _FilterType, string _InitialDirectory)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (_FilterType == "zip")
+            {
+                if (dataGridView1.Rows.Count == 0)
+                {
+                    dialog.InitialDirectory = "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
+                }
+                dialog.Filter = "Zip files (*.zip)| *.zip";
+            }
+            else
+            {
+                dialog.Filter = "Installer files (*.exe; *.msi)| *.exe; *.msi";
+                dialog.InitialDirectory = _InitialDirectory;
+            }
+                        
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+               return dialog.FileName;
+            }
+            return null;
+        }
+        //Unzip file to folder
+        private void _UnzipFileTo(string source, string destination)
+        {
+            if (Directory.Exists(destination))
+            {
+                Directory.Delete(destination, true);
+            }
+            ZipFile.ExtractToDirectory(source, destination);
+        }
+
+        //Install driver via execution file
+        private void _InstallDriver (string _ExeLocation)
+        {
+            if (File.Exists(_ExeLocation + "\\setup.exe"))
+            {
+                _InstallerExeFile = _ExeLocation+"\\setup.exe";              
+            }
+
+            else if (File.Exists(_ExeLocation + "\\install.exe"))
+            {
+                _InstallerExeFile = _ExeLocation + "\\install.exe";
+            }
+
+            else
+            {
+                do
+                {
+                    _InstallerExeFile = _GetFilePath("exe", _Unzipto);
+                    if (_InstallerExeFile == null)
+                        MessageBox.Show("Please select Installer!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                while (_InstallerExeFile == null);
+            }
+            label_ProcessStatus.Text = "Installing " + _DeviceName + " driver";
+            MessageBox.Show(_InstallerExeFile);
+            //Process.Start(_InstallerExeFile, "/?");
+        }
+
+        //Hide button and lab while working
+        private void _BadUserDefander()
+        {
+            btn_add.Visible = !btn_add.Visible;
+            btn_sel.Visible = !btn_sel.Visible;
+            btn_Start.Visible = !btn_Start.Visible;
+            lab_ReadMe.Visible = !lab_ReadMe.Visible;
+            text_FilePatSel.Visible = !text_FilePatSel.Visible;
+            pictureBox_processing.Visible = !pictureBox_processing.Visible;
+            label_ProcessStatus.Visible = !label_ProcessStatus.Visible;
+            label_ProcessStatus.Text = "Unzipping " + _DeviceName + " driver";
+        }
+        //------ Button Event ------ 
+
+        //Select zip file(Select Button)
+        private void btn_sel_Click(object sender, EventArgs e)
+        {
+            _FilepathSel = _GetFilePath("zip", "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}");
+            if (_FilepathSel != null)
+            {
+                _FilepathSel = _FilepathSel.ToLower();
+                text_FilePatSel.Text = _FilepathSel;
+            }
+            
+        }
+        
+        //Delete driver in list(Datagrid Delete Button)
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
+            {
+                dataGridView1.Rows.RemoveAt(e.RowIndex);
+            }
+        }
+
+        //Add zip file path in list(Add Button)
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (_FilepathSel != null)
+            {
+                DataGridViewRowCollection row = dataGridView1.Rows;
+                //Get device naem from _FilepathSel
+                _Floderpath = Path.GetFileName(Path.GetDirectoryName(_FilepathSel));
+                //Add select zip into Datagrid
+                row.Add(new object[] { _Floderpath, _FilepathSel, "Del" });
+                _FilepathSel = text_FilePatSel.Text = null;
+                return;
+            }
+            //Shows error message if not select file before click add button
+            MessageBox.Show("Didn't select any file!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        //Start to install(Start Button)
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        public void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            backgroundWorker.ReportProgress(0);
+            try
+            {
+                _DeviceName = dataGridView1.Rows[0].Cells[0].Value.ToString();
+                _ZipPath = dataGridView1.Rows[0].Cells[1].Value.ToString();
+                _Unzipto = Path.GetDirectoryName(_ZipPath) + "\\" + Path.GetFileNameWithoutExtension(_ZipPath);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("The list is empty!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            _UnzipFileTo(_ZipPath, _Unzipto);
+            _InstallDriver(_Unzipto);
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            _BadUserDefander();
+        }
+        void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            _BadUserDefander();
+        }
+    }
+}
